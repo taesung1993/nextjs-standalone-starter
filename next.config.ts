4268958a0f78ddf31c36a455b9e238adde41c849
lumigloo_mason <>
2025-01-13 19:25:01 +0900

    add compiler option

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   assetPrefix: `${process.env.CDN_BASE_URL}/${packageVersion}`,
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
 };
 
 export default process.env.NODE_ENV === "production" ? nextConfig : {};
