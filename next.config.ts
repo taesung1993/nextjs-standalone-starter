c97d884a89ce939d61d1ef5c3c5b16b4f3a5ed2e
lumigloo_mason <>
2025-01-13 17:13:43 +0900

    add environment and modify next.config.ts for standalone

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import { version as packageVersion } from "./package.json";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: `${process.env.CDN_BASE_URL}/${packageVersion}`,
 };
 
-export default nextConfig;
+export default process.env.NODE_ENV === "production" ? {} : nextConfig;
